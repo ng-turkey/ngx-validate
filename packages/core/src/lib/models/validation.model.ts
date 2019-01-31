e338a14b8a9f2fabeca6d2f8165efcdf8bb8bc05
Levent Arman Özak <arman@armanozak.com>
2019-01-31 10:50:54 +0300

    Add models

---
+++ packages/core/src/lib/models/validation.model.ts
@@ -0,0 +1,32 @@
+import { Type } from '@angular/core';
+import { NgControl } from '@angular/forms';
+
+export namespace Validation {
+  export type Config = Partial<{
+    blueprints: Blueprints;
+    errorTemplate: Type<any>;
+    invalidClasses: string;
+    mapErrorsFn: MapErrorsFn;
+    skipValidation: boolean;
+    targetSelector: string;
+    validateOnSubmit: boolean;
+  }>;
+
+  export interface Blueprints {
+    [key: string]: string;
+  }
+
+  export interface ParamMap {
+    [key: string]: string | number | boolean | string[];
+  }
+
+  export interface Error {
+    key: string;
+    params?: ParamMap;
+    message?: string;
+  }
+
+  export type MapErrorsFn = (errors: Error[], formErrors: Error[], control?: NgControl) => Error[];
+
+  export type StreamName = 'status' | 'submit' | 'value';
+}
--- packages/core/src/public_api.ts
+++ packages/core/src/public_api.ts
@@ -3,3 +3,4 @@
  */
 
 export * from './lib/core.module';
+export * from './lib/models';
