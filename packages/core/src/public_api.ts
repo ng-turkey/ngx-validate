d4f25b26b29162733d469585946fc7b9bd3ef1b4
Levent Arman Özak <arman@armanozak.com>
2019-01-31 12:28:39 +0300

    Export abstracts, components, and directives from public_api

--- packages/core/src/public_api.ts
+++ packages/core/src/public_api.ts
@@ -3,7 +3,10 @@
  */
 
 export * from './lib/core.module';
+export * from './lib/abstracts';
+export * from './lib/components';
 export * from './lib/constants';
+export * from './lib/directives';
 export * from './lib/models';
 export * from './lib/utils';
 export * from './lib/validators';
