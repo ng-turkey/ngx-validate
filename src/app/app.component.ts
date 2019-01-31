9097887582f87d977e63e00e03182e98ad950b59
Levent Arman Özak <arman@armanozak.com>
2019-01-31 14:02:47 +0300

    Add a basic demo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,65 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { comparePasswords, validatePassword, Validation } from '../../packages/core/src/public_api';
+
+const { minLength, required, requiredTrue } = Validators;
+
+const PASSWORD_FIELDS = ['password', 'repeat'];
+
+const mapErrorsFn: Validation.MapErrorsFn = function(errors, groupErrors, control) {
+  if (PASSWORD_FIELDS.indexOf(control.name) < 0) return errors;
+
+  return errors.concat(groupErrors.filter(({ key }) => key === 'passwordMismatch'));
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
-  title = 'ngx-validate';
+  form: FormGroup;
+
+  mapErrorsFn: Validation.MapErrorsFn = mapErrorsFn;
+
+  get username(): AbstractControl {
+    return this.form.controls.username;
+  }
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      credentials: this.fb.group(
+        {
+          username: [
+            '',
+            {
+              validators: [required, minLength(3)],
+            },
+          ],
+          ...PASSWORD_FIELDS.reduce(
+            (acc, key) => ({
+              ...acc,
+              [key]: [
+                '',
+                {
+                  validators: [required, minLength(6), validatePassword(['small', 'capital', 'number'])],
+                },
+              ],
+            }),
+            {},
+          ),
+        },
+        {
+          validators: [comparePasswords(PASSWORD_FIELDS)],
+        },
+      ),
+      consent: [
+        false,
+        {
+          validators: [requiredTrue],
+        },
+      ],
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgxValidateCoreModule } from '../../packages/core/src/public_api';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, NgxValidateCoreModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent],
 })
