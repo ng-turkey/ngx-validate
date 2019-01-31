92ca92ba0eb72d4ef7dd70500e10ce94c76f0ff1
Levent Arman Özak <arman@armanozak.com>
2019-01-31 10:32:48 +0300

    Re-format every file in project

--- packages/core/src/lib/core.module.ts
+++ packages/core/src/lib/core.module.ts
@@ -3,6 +3,6 @@ import { NgModule } from '@angular/core';
 @NgModule({
   declarations: [],
   imports: [],
-  exports: []
+  exports: [],
 })
-export class NgxValidateCoreModule { }
+export class NgxValidateCoreModule {}
