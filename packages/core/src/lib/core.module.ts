a3c853ce57849eb725e4d01e1f0e561a924c5989
Levent Arman Özak <arman@armanozak.com>
2019-01-31 10:59:39 +0300

    Add a configuration for the core module

--- packages/core/src/lib/core.module.ts
+++ packages/core/src/lib/core.module.ts
@@ -1,8 +1,28 @@
-import { NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { Validation } from './models';
 
 @NgModule({
   declarations: [],
   imports: [],
   exports: [],
 })
-export class NgxValidateCoreModule {}
+export class NgxValidateCoreModule {
+  static forRoot(config = {} as Validation.Config): ModuleWithProviders {
+    return {
+      ngModule: NgxValidateCoreModule,
+      providers: [
+        {
+          provide: 'VALIDATION_CONFIG',
+          useValue: {
+            blueprints: config.blueprints,
+            errorTemplate: config.errorTemplate,
+            invalidClasses: config.invalidClasses,
+            mapErrorsFn: config.mapErrorsFn,
+            targetSelector: config.targetSelector,
+            validateOnSubmit: config.validateOnSubmit,
+          },
+        },
+      ],
+    };
+  }
+}
