0b5b3a84521a2fe20441bac643e2a1e788a0ddb3
Mahmut Gundogdu <mahmutgundogdu@hotmail.com.tr>
2022-08-29 15:03:40 +0300

    Fix test env issues

--- packages/core/src/test.ts
+++ packages/core/src/test.ts
@@ -1,11 +1,12 @@
+import 'zone.js';
+import 'zone.js/testing';
+
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting,
 } from '@angular/platform-browser-dynamic/testing';
 import 'core-js/es7/reflect';
-import 'zone.js';
-import 'zone.js/testing';
 
 declare const require: any;
 
--- src/test.ts
+++ src/test.ts
@@ -1,9 +1,11 @@
+import 'zone.js';
+import 'zone.js/testing';
+
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting,
 } from '@angular/platform-browser-dynamic/testing';
-import 'zone.js/testing';
 
 declare const require: any;
 
